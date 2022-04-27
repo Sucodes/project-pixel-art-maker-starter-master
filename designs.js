6863b0267223f7e6d5bc06f05c0e37f246043584
Oghenesuvwe Money <rhounamoney@gmail.com>
2022-04-27 14:57:54 +0100

    Add comments

--- designs.js
+++ designs.js
@@ -7,31 +7,33 @@ const width = document.getElementById("inputWidth");
 const canvas = document.getElementById("pixelCanvas"); // Canvas for pixel display
 
 var submitButtonInput = document.getElementById("submitButton");
+
 // When size is submitted by the user, call makeGrid()
+
 submitButtonInput.addEventListener("click", function (event) {
-    event.preventDefault();
+    event.preventDefault(); // Block the initial submit action
     clearGrid();
     makeGrid();
 }); 
 
 function clearGrid(){
-    while (canvas.firstChild) {
+    while (canvas.firstChild) { // Loop over canvas and remove all children
         canvas.removeChild(canvas.firstChild);
     }
 }
 
 function makeGrid() {
     for(var r = 0; r < height.value; r++){
-        const row = document.createElement("tr");
+        const row = document.createElement("tr"); //Define table rows
         
         canvas.appendChild(row);
 
         for(var c = 0; c < width.value; c++) {
-         const col = document.createElement("td"); 
+         const col = document.createElement("td"); //Define table columns
          
          row.appendChild(col);
         
-         col.addEventListener("click", function() {
+         col.addEventListener("click", function() { // Add color event
             col.style.backgroundColor = color.value;
          });
         }
